e0fe34ded66eaee2ab6967a971d5f1e830311640
Essrinc <sabireesraozcan@gmail.com>
2022-10-20 13:33:16 +0300

    Add project files.

---
+++ NLayer.Repository/AddDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        //veritabanı yolunu startup dosyasından vereceğim için options alan ctor oluşturdum
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            //yazılımcı productFeatures eklemek istersen product üzerinde eklesin.
+            //var p = new Product() {ProductFeature= new ProductFeature() { } }
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            modelBuilder.Entity<ProductFeature>().HasData(new ProductFeature { Id = 1, Color = "Kırmızı", Height = 100, Width = 200, ProductId = 1 },
+                new ProductFeature { Id = 2, Color = "Mavi", Height = 300, Width = 500, ProductId = 2 });
+        }
+    }
+}
